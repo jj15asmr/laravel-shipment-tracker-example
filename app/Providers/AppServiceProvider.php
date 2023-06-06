7bfa6f85c800ef031271e895dfebe9e2152093c5
Jacob Daniel <jacob.daniel@asmrtistunited.com>
2023-06-06 13:39:56 -0400

    Remove Str macro from AppServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,17 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Str::macro('randomDigits', function(int $length) {
-            $min = (int) str_repeat('0', $length);
-            $max = (int) str_repeat('9', $length);
-
-            $random_digits = random_int($min, $max);
-
-            if (str($random_digits)->length() < $length) {
-                $random_digits = (string) str($random_digits)->padLeft($length, '0');
-            }
-
-            return $random_digits;
-        });
+        //
     }
 }
